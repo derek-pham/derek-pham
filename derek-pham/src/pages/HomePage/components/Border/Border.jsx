807f91c4570c0c59dc1a64fe081a7208b6ebfe71
derek-pham <derekpham3@gmail.com>
2024-04-29 19:18:43 +1000

    - fixed zindex issues

--- derek-pham/src/pages/HomePage/components/Border/Border.jsx
+++ derek-pham/src/pages/HomePage/components/Border/Border.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import Header from "../Header/Header";
 
-function Border() {
+function Border({ children }) {
 
     // In your component
     return (
         <>
             <div style={{
                 position: 'absolute',
-                zIndex: 0,
+                zIndex: -1,
                 height: 'calc((100vh - 20px)/2)',
-                width: 'calc(100vw - 20px)',
+                width: 'calc(100% - 20px)',
                 border: '5px solid #242424',
                 borderBottom: 'none',
                 borderRadius: '10px 10px 0 0',
@@ -19,16 +20,22 @@ function Border() {
                 left: '0px'
             }} />
             <div style={{
-                position: 'absolute',
-                zIndex: -1,
-                height: 'calc(100vh - 20px)',
-                width: 'calc(100vw - 20px)',
+                position: 'relative',
+                zIndex: -2,
+                height: 'calc(100%)',
+                paddingTop: '40px',
+                width: 'calc(100% - 20px)',
                 border: '5px solid wheat',
                 borderRadius: '10px',
                 margin: '10px',
                 boxSizing: 'border-box',
                 top: '0px',
                 left: '0px'
+            }}>
+                {children}
+            </div>
+            <div style={{
+                height: '20px'
             }} />
         </>
 
