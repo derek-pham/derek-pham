e345505527984feb3e58aa41d1d83121838db379
derek-pham <derekpham3@gmail.com>
2024-04-29 19:16:53 +1000

    - created Quotes section

---
+++ derek-pham/src/pages/HomePage/components/Quotes/Quotes.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Quotes.css'
+
+function Quotes() {
+
+    return (
+        <>
+            <div className='quotes-container'>
+                <p>QUOTE: <p style={{ fontStyle: "italic" }}>"If you want something you have never had,<br /> you must be willing to do something you have never done."— Anonymous </p></p>
+            </div>
+        </>
+    );
+
+}
+
+export default Quotes
