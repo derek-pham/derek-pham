37c68f06a8b6c280e34501501b82d29d03ce855d
derek-pham <derekpham3@gmail.com>
2024-04-29 15:10:32 +1000

    - basic paragraph

---
+++ derek-pham/src/pages/HomePage/components/SkillsAndProjects/SkillsAndProjects.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+// import './SkillsAndProjects.css'
+
+function SkillsAndProjects() {
+
+    return (
+        <>
+            <div className='place3'>
+                <h1>Skills & Projects</h1>
+                <div className="skillsandprojects-text">
+                    <p>
+                        With a robust foundation in both front-end and back-end development, my technical expertise spans HTML, CSS, JavaScript, TypeScript, Node.js, MongoDB, and React. A standout project from my portfolio is 'BiteMap', a capstone project developed during my software engineering course at UTS in collaboration with trainers and mentors from the Institute of Data. This project earned the 'Top Capstone Project' award and allowed me to refine my skills in integrating diverse technologies to enhance user experiences. Moreover, I am also proficient in using tools like Git for version control to streamline project workflows and enhance team collaboration. I am actively expanding my expertise in emerging technologies and advancements in software development to ensure my skills remain ready to meet the challenges of future projects.
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+
+}
+
+export default SkillsAndProjects
