54723730f87c50f270c58e106532bea4def3ecbf
derek-pham <derekpham3@gmail.com>
2024-04-29 15:10:16 +1000

    - basic Profile section

---
+++ derek-pham/src/pages/HomePage/components/Profile/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Profile.css'
+
+function Profile() {
+
+    return (
+        <>
+            <div className="profile-container">
+                <h1>Derek Pham</h1>
+                <div className="socials-container">
+                    <a href="https://github.com/derek-pham" target="_blank">Github</a>
+                    <a href="https://www.linkedin.com/in/derek-pham-5605661a2/" target="_blank">LinkedIn</a>
+                </div>
+            </div>
+
+        </>
+    );
+
+}
+
+export default Profile
