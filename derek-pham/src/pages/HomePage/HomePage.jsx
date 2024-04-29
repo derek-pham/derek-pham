b58404d65f7b5284252351be6039c1b07bf9aeae
derek-pham <derekpham3@gmail.com>
2024-04-29 19:18:14 +1000

    - removed placeholders

--- derek-pham/src/pages/HomePage/HomePage.jsx
+++ derek-pham/src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,25 @@
 import './HomePage.css'
-import './responsive-css/responsive.css'
 import BackgroundAccent from './components/BackgroundAccent/BackgroundAccent'
 import Border from './components/Border/Border'
 import Header from './components/Header/Header'
 import Profile from './components/Profile/Profile'
+import Quotes from './components/Quotes/Quotes'
 import About from './components/About/About'
 import SkillsAndProjects from './components/SkillsAndProjects/SkillsAndProjects'
 
 export default function HomePage() {
     return (
         <>
-        <BackgroundAccent />
-        <Border />       
-        <Header />
-        <Profile />
-        <About />
-        <SkillsAndProjects />
-        <div className='place1'>            <p>QUOTE: <p style={{fontStyle:"italic"}}>"If you want something you have never had,<br /> you must be willing to do something you have never done."— Anonymous </p></p></div>
-        <div className='place4'>            <p>CONTACT: <p style={{fontStyle:"italic"}}>text text text text text text text text text text text text text text text</p></p></div>
+            <div className='homepage'>
+                <BackgroundAccent />
+                <Header />
+                <Border>
+                    <Profile />
+                    <Quotes />
+                    <About />
+                    <SkillsAndProjects />
+                </Border>
+            </div >
         </>
     )
 }
