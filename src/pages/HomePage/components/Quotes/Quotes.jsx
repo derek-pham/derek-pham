fe627994f629ba3df1b62a5f7605d48d52bcfa6e
derek-pham <derekpham3@gmail.com>
2024-05-01 17:30:02 +1000

    - added auto-updating quotes

--- src/pages/HomePage/components/Quotes/Quotes.jsx
+++ src/pages/HomePage/components/Quotes/Quotes.jsx
@@ -1,16 +1,45 @@
-import React from "react";
-import './Quotes.css'
+import React, { useState, useEffect } from "react";
+import './Quotes.css';
+import { quoteData } from "./quoteData";
 
 function Quotes() {
+    const [currentQuote, setCurrentQuote] = useState('');
+    const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+
+    useEffect(() => {
+        const updateQuote = () => {
+            if (currentQuoteIndex >= quoteData.length - 1) {
+                setCurrentQuoteIndex(0); // Reset index to 0 if it's the last quote
+            } else {
+                setCurrentQuoteIndex(currentQuoteIndex + 1);
+            }
+            setCurrentQuote(quoteData[currentQuoteIndex]['quote']);
+        };
+
+        // Initially set the quote
+        if (currentQuote === '') {
+            setCurrentQuote(quoteData[currentQuoteIndex]['quote'])
+        }
+
+        // Interval here to auto-update the quotes 
+        const intervalId = setInterval(updateQuote, 10000); // Update every 10 seconds
+
+        // Cleanup function to clear interval when component unmounts
+        return () => clearInterval(intervalId);
+    }, [currentQuoteIndex]); // Depend on currentQuoteIndex to trigger update
 
     return (
         <>
             <div className='quotes-container'>
-                <p>QUOTE: <p style={{ fontStyle: "italic" }}>"If you want something you have never had,<br /> you must be willing to do something you have never done."— Anonymous </p></p>
+                <p>
+                    " {currentQuote} "&nbsp;
+                </p>
+                <p>
+                    — {quoteData[currentQuoteIndex]['speaker']}
+                </p>
             </div>
         </>
     );
-
 }
 
-export default Quotes
+export default Quotes;
---
+++ src/pages/HomePage/components/Quotes/quoteData.js
@@ -0,0 +1,21 @@
+export const quoteData = [
+  {
+    quote: "The cave you fear to enter holds the treasure you seek.",
+    speaker: "Joseph Campbell",
+  },
+  {
+    quote:
+      "The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty.",
+    speaker: "Winston Churchill",
+  },
+  {
+    quote:
+      "If you want something you have never had, you must be willing to do something you have never done.",
+    speaker: "Anonymous",
+  },
+  {
+    quote:
+      "What you get by achieving your goals is not as important as what you become by achieving your goals.",
+    speaker: "Henry David Thoreau",
+  }
+];
