1a683efe997ae04aa55ab41bc4f35b0015906a1d
derek-pham <derekpham3@gmail.com>
2024-05-06 16:54:40 +1000

    - added fade animations to quote rotations

--- src/pages/HomePage/components/Quotes/Quotes.jsx
+++ src/pages/HomePage/components/Quotes/Quotes.jsx
@@ -1,17 +1,27 @@
-import React, { useState, useEffect } from "react";
-import "./Quotes.css";
-import { quoteData } from "./quoteData";
+import React, { useState, useEffect } from 'react';
+import './Quotes.css';
+import { quoteData } from './quoteData';
+import 'animate.css';
 
 function Quotes() {
   const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+  const [fade, setFade] = useState(true); // State to handle fade animation
 
   useEffect(() => {
     // Function to update quote index
     const updateQuote = () => {
-      setCurrentQuoteIndex(prevIndex => {
-        const nextIndex = prevIndex >= quoteData.length - 1 ? 0 : prevIndex + 1;
-        return nextIndex;
-      });
+      // Trigger fade out animation
+      setFade(false);
+
+      setTimeout(() => {
+        setCurrentQuoteIndex(prevIndex => {
+          const nextIndex = prevIndex >= quoteData.length - 1 ? 0 : prevIndex + 1;
+          return nextIndex;
+        });
+
+        // Trigger fade in animation
+        setFade(true);
+      }, 500); // Delay should be less than the interval time
     };
 
     // Set the interval to update the quote every 10 seconds
@@ -23,8 +33,13 @@ function Quotes() {
 
   return (
     <div className="quotes-container">
-      <p id="quote-text">" {quoteData[currentQuoteIndex].quote} " &nbsp;</p> <br />
-      <p id="speaker">— {quoteData[currentQuoteIndex].speaker}</p>
+      <p id="quote-text" className={fade ? 'animate__animated animate__fadeIn' : 'animate__animated animate__fadeOut'}>
+        "{quoteData[currentQuoteIndex].quote}" &nbsp;
+      </p>
+      <br />
+      <p id="speaker" className={fade ? 'animate__animated animate__fadeIn' : 'animate__animated animate__fadeOut'}>
+        — {quoteData[currentQuoteIndex].speaker}
+      </p>
     </div>
   );
 }
