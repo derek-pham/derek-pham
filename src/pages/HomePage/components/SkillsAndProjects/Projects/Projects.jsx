ce283e0c8f09e387cf940ccd013b478d4b40d09d
derek-pham <derekpham3@gmail.com>
2024-05-27 17:26:48 +1000

    - added two new projects

--- src/pages/HomePage/components/SkillsAndProjects/Projects/Projects.jsx
+++ src/pages/HomePage/components/SkillsAndProjects/Projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import './Projects.css'
 import bitemapIcon from './icons/bitemap-icon.png'
+import codeBrewCafeIcon from './icons/codebrewcafe.png'
+import recipeRumbleIcon from './icons/reciperumble.png'
 import wipIcon from './icons/work-in-progress.png'
 import starIcon from './icons/star.png'
 
@@ -12,6 +14,14 @@ function Projects({ onSkillEnter, onSkillLeave }) {
         setTriggerAward(true)
         setTriggerStar(true)
         onSkillEnter('BiteMap')
+    }
+
+    const triggerCodeBrewCafe = () => {
+        onSkillEnter('CodeBrewCafe')
+    }
+
+    const triggerRecipeRumble = () => {
+        onSkillEnter('RecipeRumble')
     }
 
     const handleMouseLeave = () => {
@@ -29,6 +39,16 @@ function Projects({ onSkillEnter, onSkillLeave }) {
                     </a>
                     {triggerStar ? <img src={starIcon} alt="Star" id="starIcon" /> : ''}
                     {triggerAward ? <p id="award-text">TOP CAPSTONE AWARD</p> : ''}
+                </div>
+                <div id="codebrewcafe" className="project" onMouseEnter={triggerCodeBrewCafe} onMouseLeave={handleMouseLeave}>
+                    <a href="https://github.com/derek-pham/CodeBrewCafe" target="_blank">
+                        <img src={codeBrewCafeIcon} alt="CodeBrewCafe" />
+                    </a>
+                </div>
+                <div id="reciperumble" className="project" onMouseEnter={triggerRecipeRumble} onMouseLeave={handleMouseLeave}>
+                    <a href="https://github.com/derek-pham/RecipeRumble" target="_blank">
+                        <img src={recipeRumbleIcon} alt="RecipeRumble" />
+                    </a>
                 </div>
                 <div id="wip">
                     <img src={wipIcon} alt="WIP" />
